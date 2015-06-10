43de0b479ff4b2afcd02ec4a7d50d3036717caba
SHINfjw <hrisy.112025@gmail.com>
2015-06-10 13:02:21 +0900

    とりあえずファイルあげてきます

---
+++ View/Member/reserve_histrory.php
@@ -0,0 +1,24 @@
+<!-- ▼表示▼ -->
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<!-- 定義 -->
+		<meta http-equiv="content-type" content="text/html; charset=utf-8">
+		<meta name="viewport" content="width=device-width,initial-scale=1.0">
+		<meta http-equiv="content-style-type" content="text/css">
+		<meta http-equiv="Content-Script-Type" content="text/javascript" />
+		<!-- 読み込み -->
+        <link rel="stylesheet" href="../css/.css">
+
+		<title></title>
+	</head>
+	<body>
+		
+		<main>
+			<article>
+			</article>
+		</main>
+
+	</body>
+</html>
+<!-- ▲表示▲ -->
