8fc758dfcb49301a67b3a3e2dcd4aa2b3157f9c5
SHINfjw <hrisy.112025@gmail.com>
2015-06-15 18:32:10 +0900

    大幅に変わってます

--- View/Reserve/step1.php
+++ View/Reserve/step1.php
@@ -8,17 +8,50 @@
 		<meta http-equiv="content-style-type" content="text/css">
 		<meta http-equiv="Content-Script-Type" content="text/javascript" />
 		<!-- 読み込み -->
-        <link rel="stylesheet" href="../css/.css">
+      <link rel="stylesheet" href="../css/.css">
 
 		<title></title>
 	</head>
 	<body>
-		
+		<!-- ヘッダー -->
+<?php
+	require_once "../parts/header.html";
+?>
+
 		<main>
-			<article>
-			</article>
+			<!-- パンくずリスト -->
+			<div class="bread">
+			</div>
+
+			<div>
+				<article>
+					<!-- 上映スケジュール -->
+					<div>
+
+					</div>
+					<!-- 日付曜日 -->
+					<div>
+					</div>
+
+					<div>
+						<sectio>
+							<h3>タイトル</h3>
+							<img src="" alt="">
+							<ul>
+								<li>スケジュール</li>
+							</ul>
+						<section>
+					</div>
+				</article>
+				<aside>
+				</aside>
+			</div>
 		</main>
 
+ <!-- フッター -->
+<?php
+	require_once "../parts/footer.html";
+?>
 	</body>
 </html>
 <!-- ▲表示▲ -->
--- View/Shop/schedule.php
+++ View/Shop/schedule.php
@@ -8,15 +8,82 @@
 		<meta http-equiv="content-style-type" content="text/css">
 		<meta http-equiv="Content-Script-Type" content="text/javascript" />
 		<!-- 読み込み -->
-        <link rel="stylesheet" href="../css/.css">
+		<link rel="stylesheet" href="../../css/Base/normalize.css">
+		<link rel="stylesheet" href="../../css/Base/base.css">
+		<link rel="stylesheet" href="../../css/font-awesome.css">
 
-		<title></title>
+		<link rel="stylesheet" href="../../css/Shop/schedule.css">
+		<link rel="stylesheet" href="../../css/layout.css">
+		<link rel="stylesheet" href="../../css/Common/common.css">
+
 	</head>
 	<body>
-		<main>
-			<article>
-			</article>
-		</main>
+		<div class="l-wrapper">
+			<!-- ヘッダー -->
+<?php
+	require_once "../parts/header.html";
+?>
+
+			<main class="l-content">
+				<!-- ▼パンくず -->
+				<ul class="l-breadcrumb">
+						<li><a href="">HOME</a></li>
+				</ul>
+				<!-- ▲パンくず -->
+
+				<!-- ▼メインコンテンツ -->
+				<article class="l-cLeft">
+					<h1>上映スケジュール</h1>
+
+					<!-- ▼日付選択 -->
+					<div class="day_Select float_C">
+
+						<a href="#" class="float_L"><i class="fa fa-angle-left"></i></a>
+						<ul class="">
+<?php
+	for( $i=0;$i<7;$i++ ){
+?>
+							<li><a href="#">日付</a></li>
+<?php
+	}
+?>
+						</ul>
+						<a href="#" class="float_R"><i class="fa fa-angle-right"></i></a>
+
+					</div>
+					<!-- ▲日付選択 -->
+
+					<!-- ▼作品表示 -->
+					<div class="schedule">
+						<h2 class="text_Center">mm/ddの上映スケジュール</h2>
+
+						<!-- ▼スケジュール -->
+						<section class="float_C">
+							<a href="#"><h3>作品タイトル</h3>
+							<img src="" alt="作品タイトル" class="float_L"></a>
+							<!-- ◎→余裕あり △→あと少し ×→なし -->
+							<ul class="float_C text_Center">
+								<li><a href="#"><div class="day_Time">hh:mm<br/>～<br/>hh:mm</div><div class="reserve_Ok">◎予約購入</div></a></li>
+								<li><a href="#"><div class="day_Time">hh:mm<br/>～<br/>hh:mm</div><div class="reserve_Early">△予約購入</div></a></li>
+								<li><a href="#"><div class="day_Time">hh:mm<br/>～<br/>hh:mm</div><div class="reserve_No">×予約購入</div></a></li>
+							</ul>
+						</section>
+						<!-- ▲スケジュール -->
+
+					</div>
+					<!-- ▲作品表示 -->
+				</article>
+				<!-- ▲メインコンテンツ -->
+<?php
+	require_once "../parts/right_side.html";
+?>
+			</main>
+
+ <!-- フッター -->
+<?php
+	require_once "../parts/footer.html";
+?>
+		</div>
 	</body>
 </html>
 <!-- ▲表示▲ -->
