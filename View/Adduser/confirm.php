1ee1c3e6c20ac1e78c0d727f473c4670bbca4efd
koichi <koichi.ehara@gmail.com>
2015-06-29 12:59:40 +0900

    新規登録確認画面作成

--- View/Adduser/confirm.php
+++ View/Adduser/confirm.php
@@ -8,14 +8,145 @@
 		<meta http-equiv="content-style-type" content="text/css">
 		<meta http-equiv="Content-Script-Type" content="text/javascript" />
 		<!-- 読み込み -->
-        <link rel="stylesheet" href="../css/.css">
+		<link rel="stylesheet" href="../.././css/Bace/normalize.css">
+		<link rel="stylesheet" href="../.././css/Bace/base.css">
+		<link rel="stylesheet" href="../.././css/layout.css">
+		<link rel="stylesheet" href="../.././css/Common/common.css">
+		<link rel="stylesheet" href="./css/step1.css">
 
-		<title></title>
+		<title>新規登録確認</title>
 	</head>
 	<body>
-		
+
 		<main>
-			<article>
+			<article class="l-wrapper">
+				<section class="l-stepHead">
+					<h1>新規登録確認</h1>
+				</section>
+
+				<section class="l-stepMain">
+
+					<form action="complete.php" method="post" class="userForm">
+						<table>
+							<tr>
+								<td colspan="2">
+									会員登録内容
+								</td>
+							</tr>
+							<tr>
+								<td class="l-tdWidth1">
+									ユーザネーム
+								</td>
+								<td class="l-tdWidth2">
+									えはら
+								</td>
+							</tr>
+							<tr>
+								<td>
+									ユーザID
+								</td>
+								<td>
+									koichi123
+								</td>
+							</tr>
+							<tr>
+								<td>
+									メールアドレス
+								</td>
+								<td>
+									101hundredone@gmail.com
+								</td>
+							</tr>
+							<tr>
+								<td>
+									パスワード
+								</td>
+								<td>
+									●●●●●●●
+								</td>
+							</tr>
+							<tr>
+								<td rowspan="2">
+									お名前
+								</td>
+								<td class="username">
+									<span>江原</span>
+									孝一
+								</td>
+							</tr>
+							<tr>
+								<td class="username">
+									<span>エハラ</span>
+									コウイチ
+								</td>
+							</tr>
+							<tr>
+								<td>
+									電話番号
+								</td>
+								<td>
+									09019799681
+								</td>
+							</tr>
+							<tr>
+								<td>
+									性別
+								</td>
+								<td>
+									男
+								</td>
+							</tr>
+							<tr>
+								<td>
+									生年月日
+								</td>
+								<td>
+									19940620
+								</td>
+							</tr>
+							<tr>
+								<td>
+									都道府県
+								</td>
+								<td>
+									千葉県
+								</td>
+							</tr>
+						</table>
+
+						<table>
+							<tr>
+								<td colspan="2">
+									お支払方法
+								</td>
+							</tr>
+							<tr>
+								<td>
+									コンビニ
+								</td>
+								<td>
+									セブンイレブン
+								</td>
+							</tr>
+							<tr>
+								<td>
+									店舗
+								</td>
+								<td>
+									千葉県市川市南八幡 駅前店
+								</td>
+							</tr>
+						</table>
+
+						<div class="l-submitBox">
+							<p>
+								入力内容に間違いがなければ「登録する」ボタンをクリックしてください
+							</p>
+						<input type="submit" placeholder="登録する">
+						</div>
+
+					</form>
+				</section>
 			</article>
 		</main>
 
