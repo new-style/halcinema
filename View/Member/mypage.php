efd5f328911ebfa27fd3b9cb3f2a5b972fc669a2
koichi <koichi.ehara@gmail.com>
2015-06-29 12:12:38 +0900

    マイページ

--- View/Member/mypage.php
+++ View/Member/mypage.php
@@ -8,14 +8,56 @@
 		<meta http-equiv="content-style-type" content="text/css">
 		<meta http-equiv="Content-Script-Type" content="text/javascript" />
 		<!-- 読み込み -->
-        <link rel="stylesheet" href="../css/.css">
+        <link rel="stylesheet" href="../.././css/Bace/normalize.css">
+        <link rel="stylesheet" href="../.././css/Bace/base.css">
+        <link rel="stylesheet" href="../.././css/layout.css">
+        <link rel="stylesheet" href="../.././css/Common/common.css">
+        <link rel="stylesheet" href="./css/mypage.css">
 
-		<title></title>
+		<title>マイページ</title>
 	</head>
 	<body>
-		
+
 		<main>
-			<article>
+			<article class="l-wrapper">
+				<section class="l-myContent clearfix">
+					<div class="box float_L">
+						<p>
+							予約履歴/購入履歴
+						</p>
+					</div>
+
+					<div class="box float_L">
+						<p>
+							お気に入りリスト
+						</p>
+					</div>
+
+					<div class="box float_L">
+						<p>
+							ポイント(決済ポイント、ログインポイント)
+						</p>
+					</div>
+
+					<div class="box2 float_L">
+						<p>
+							キャンペーン情報
+						</p>
+					</div>
+
+					<div class="box float_L">
+						<p>
+							シネマズマガジン
+						</p>
+					</div>
+
+					<div class="box float_L">
+						<p>
+							ユーザー情報登録(クレジット情報など)
+						</p>
+					</div>
+
+				</section>
 			</article>
 		</main>
 
