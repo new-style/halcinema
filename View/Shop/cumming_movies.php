5f9b9c0ec185fbf6a235228c3da240dddf13d20d
koichi123 <101hundredone@gmail.com>
2015-09-16 11:45:10 +0900

    完了ページ、公開予定作品のファイル名変更しました

---
+++ View/Shop/cumming_movies.php
@@ -0,0 +1,74 @@
+<!-- ▼表示▼ -->
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<title></title>
+		<!-- 定義 -->
+		<meta http-equiv="content-type" content="text/html; charset=utf-8">
+		<meta name="viewport" content="width=device-width,initial-scale=1.0">
+		<meta http-equiv="content-style-type" content="text/css">
+		<meta http-equiv="Content-Script-Type" content="text/javascript" />
+		<!-- 読み込み -->
+		<link rel="stylesheet" href="./../../css/Base/normalize.css">
+		<link rel="stylesheet" href="./../../css/Base/base.css">
+		<link rel="stylesheet" href="./../../css/font-awesome.css">
+
+		<link rel="stylesheet" href="./../../css/layout.css">
+		<link rel="stylesheet" href="./../../css/Common/common.css">
+		<link rel="stylesheet" href="./../../css/Shop/comming_movies.css">
+
+	</head>
+	<body>
+		<div class="l-wrapper">
+			<!-- ▼ヘッダー -->
+<?php
+	require_once "./../parts/header.html";
+?>
+
+			<main class="l-content">
+
+				<!-- ▼パンくず -->
+				<ul class="l-breadclumb">
+					<li><a href="http://127.0.0.1/NEWSTYLE/halcinema/View/Shop/shop_index.php">HOME</a></li>
+					<li><a href="#"></a></li>
+				</ul>
+				<!-- ▼パンくず -->
+
+				<!-- ▼左コンテンツ -->
+				<article class="l-cLeft">
+
+					<h1 class="text_Center border_Solid">公開予定の作品一覧</h1>
+
+<?php
+	for( $i=0;$i<7;$i++ ){
+?>
+					<section class="float_C border_Solid">
+						<a href="../movies_info.php" class="link_None"><h3>作品タイトル</h3>
+						<img src="" alt="作品タイトル" class="float_L"/></a>
+						<div class="info">
+							<p>
+								aaaaaaaaaaaaaaaaaaaaaaaa
+							</p>
+						</div>
+					</section>
+<?php
+	}
+?>
+				</article>
+				<!-- ▲左コンテンツ -->
+				<!-- ▼右コンテンツ -->
+<?php
+	require_once "./../parts/right_side.html";
+?>
+				<!-- ▲右コンテンツ -->
+
+			</main>
+
+ <!-- ▼フッター -->
+<?php
+	require_once "./../parts/footer.html";
+?>
+		</div>
+	</body>
+</html>
+<!-- ▲表示▲ -->
