ebe8887963392285b6de7c206d2bbd43ac1217b3
koichi123 <101hundredone@gmail.com>
2015-09-28 14:59:46 +0900

    halcinemaのトップページ(index.php)を変更しました

--- View/index.php
+++ View/index.php
@@ -13,32 +13,78 @@
 		<link rel="stylesheet" href="./../css/Base/base.css">
 		<link rel="stylesheet" href="./../css/font-awesome.css">
 
-		<link rel="stylesheet" href="./../css/layout.css">
-		<link rel="stylesheet" href="./../css/Common/common.css">
+		<link rel="stylesheet" href="./../css/top.css">
+		<!-- <link rel="stylesheet" href="./../css/Common/common.css"> -->
 
 	</head>
 	<body>
 		<div class="l-wrapper">
-			<!-- ▼ヘッダー -->
-<?php
-	require_once "./parts/header.html";
-?>
+		<!-- ▼ヘッダー -->
+<header class="l-header">
+	<!-- ロゴなど -->
+	<div class="l-Lheader">
+		<a href="http://127.0.0.1/halcinema/View/Shop/shop_index.php">
+			<img src="http://127.0.0.1/halcinema/img/logo.png" alt="HALシネマ TOKYO">
+		</a>
+		<a href="#">
+			<img src="" alt="広告バナー">
+		</a>
+	</div>
+
+	<div class="l-Rheader">
+		<div class="l-R-Theader">
+			<a href="http://127.0.0.1/halcinema/View/Shop/access.php">アクセス</a>
+			<a href="http://127.0.0.1/halcinema/View/how_buy.php">購入方法</a>
+			<a href="http://127.0.0.1/halcinema/View/faq.php">よくある質問</a>
+		</div>
+
+		<ul class="l-R-Bheader">
+			<li><a href="http://127.0.0.1/halcinema/View/Adduser/step1.php">新規会員登録</a></li>
+			<li><a href="http://127.0.0.1/halcinema/View/login.php">ログイン</a></li>
+		</ul>
+	</div>
+</header>
 
 			<main class="l-content">
 
 				<!-- ▼コンテンツ -->
 				<article>
-					<a href="Shop/shop_index.php" class="link_None btn btn_Regular border_Solid">東京</a>
-					<a href="#" class="link_None btn btn_Regular border_Solid">大阪</a>
-					<a href="#" class="link_None btn btn_Regular border_Solid">名古屋</a>
+					<div class="shop_Select">
+						<a href="Shop/shop_index.php" class="link_None btn btn_Regular border_Solid">東京</a>
+						<a href="#" class="link_None btn btn_Regular border_Solid">大阪</a>
+						<a href="#" class="link_None btn btn_Regular border_Solid">名古屋</a>
+					</div>
 				</article>
 				<!-- ▲コンテンツ -->
 			</main>
 
  <!-- ▼フッター -->
-<?php
-	require_once "./parts/footer.html";
-?>
+<footer class="l-footer float_C">
+<div class="float_C">
+	
+	<div class="footer_Logo float_L">
+			<img src="http://127.0.0.1/halcinema/img/logo.png" alt="HALシネマ TOKYO" height="80px">
+	</div>
+
+
+	<div class="l-footer_Sitemap_Bottom float_L float_C">
+		<ul class="sitemap_B_Ul">
+			<li class="float_L"><a href="http://127.0.0.1/halcinema/View/sitemap.php">会社概要</a></li>
+			<li class="float_L"><a href="http://127.0.0.1/halcinema/View/sitemap.php">採用情報</a></li>
+			<li class="float_L"><a href="http://127.0.0.1/halcinema/View/sitemap.php">企業･団体の方々へ</a></li>
+			<li class="float_L"><a href="http://127.0.0.1/halcinema/View/privacy.php">個人情報の取り扱いについて</a></li>
+			<li class="float_L"><a href="http://127.0.0.1/halcinema/View/sitemap.php">サイトマップ</a></li>
+			<li class="float_L"><a href="http://127.0.0.1/halcinema/View/sitemap.php">お問い合わせ</a></li>
+			<li class="float_L"><a href="http://127.0.0.1/halcinema/View/sitemap.php">特定商取引法に基づく表記</a></li>
+		</ul>
+	</div>
+</div>
+
+	<div class="copyRight">
+		<small>copyright &copy; 2015 New-style AllRights Reserved.</small>
+	</div>
+</footer>
+
 		</div>
 	</body>
 </html>
