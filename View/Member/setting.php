f3960c78893f220ed12e034b5a8bbb79bb9327dd
田中俊人 <tanaka.toshihito0521@gmail.com>
2016-03-17 17:26:02 +0900

    ユーザー登録のフォームを追加しました

--- View/Member/setting.php
+++ View/Member/setting.php
@@ -53,7 +53,7 @@
 					<div class="inn-wrapper">
 
 						<h2 class="set-H2">会員登録内容変更</h2>
-						<form action="complete.php" method="post" class="userForm">
+
 							<table>
 								<tr>
 									<td colspan="2">
@@ -65,10 +65,12 @@
 										ユーザーID
 									</td>
 									<td>
-										<input type="text" name="id" value="<?php echo $row['user_id']?>"><span class="inputMsg">※半角英数</span>
+										<form action="sys_setting.php" method="post" class="userForm">
+										<input type="text" name="user_id" value="<?php echo $row['user_id']?>"><span class="inputMsg">※半角英数</span>
 									</td>
 									<td>
-										<button>変更</button>
+										<button type="submit">変更</button>
+										</form>
 									</td>
 								</tr>
 								<tr>
@@ -76,10 +78,12 @@
 										メールアドレス
 									</td>
 									<td>
-										<input type="text" name="address" value="<?php echo $row['mail_address']?>">
+										<form action="sys_setting.php" method="post" class="userForm">
+										<input type="text" name="mail_address" value="<?php echo $row['mail_address']?>">
 									</td>
 									<td>
-										<button>変更</button>
+										<button type="submit">変更</button>
+										</form>
 									</td>
 								</tr>
 								<tr>
@@ -87,12 +91,14 @@
 										パスワード
 									</td>
 									<td>
+										<form action="sys_setting.php" method="post" class="userForm">
 										<input type="password" name="password" placeholder="古いパスワード">
 										<input type="password" name="newPassword" placeholder="新しいパスワード">
 										<input type="password" name="NnewPassword" placeholder="新しいパスワード(再入力)">
 									</td>
 									<td>
-										<button>変更</button>
+										<button type="submit">変更</button>
+										</form>
 									</td>
 								</tr>
 								<tr>
@@ -100,10 +106,12 @@
 										お名前
 									</td>
 									<td class="username">
-										<input type="text" name="familyName" value="<?php echo $row['name']?>">
+										<form action="sys_setting.php" method="post" class="userForm">
+										<input type="text" name="name" value="<?php echo $row['name']?>">
 									</td>
 									<td>
-										<button>変更</button>
+										<button type="submit">変更</button>
+										</form>
 									</td>
 								</tr>
 								<tr>
@@ -111,10 +119,12 @@
 										フリガナ
 									</td>
 									<td class="username">
-										<input type="text" name="familyNameKana" value="<?php echo $row['kana_name']?>">
+										<form action="sys_setting.php" method="post" class="userForm">
+										<input type="text" name="nameKana" value="<?php echo $row['kana_name']?>">
 									</td>
 									<td>
-										<button>変更</button>
+										<button type="submit">変更</button>
+										</form>
 									</td>
 								</tr>
 								<tr>
@@ -122,10 +132,12 @@
 										電話番号
 									</td>
 									<td>
+										<form action="sys_setting.php" method="post" class="userForm">
 										<input type="text" name="tel" value="<?php echo $row['tel']?>">
 									</td>
 									<td>
-										<button>変更</button>
+										<button type="submit">変更</button>
+										</form>
 									</td>
 								</tr>
 								<tr>
@@ -149,7 +161,6 @@
 										生年月日
 									</td>
 									<td>
-										<!-- 値入れる-->
 										<?php echo $row['birth']?>
 									</td>
 								</tr>
@@ -158,60 +169,62 @@
 										都道府県
 									</td>
 									<td>
+										<form action="sys_setting.php" method="post" class="userForm">
 										<select name="Prefectures">
 											<option value="">都道府県の選択</option>
+											<option value="北海道">北海道</option>
+											<option value="青森県">青森県</option>
+											<option value="岩手県">岩手県</option>
+											<option value="宮城県">宮城県</option>
+											<option value="秋田県">秋田県</option>
+											<option value="山形県">山形県</option>
+											<option value="福島県">福島県</option>
+											<option value="茨城県">茨城県</option>
+											<option value="栃木県">栃木県</option>
+											<option value="群馬県">群馬県</option>
+											<option value="埼玉県">埼玉県</option>
+											<option value="千葉県">千葉県</option>
+											<option value="東京都">東京都</option>
+											<option value="神奈川県">神奈川県</option>
+											<option value="新潟県">新潟県</option>
+											<option value="富山県">富山県</option>
+											<option value="石川県">石川県</option>
+											<option value="福井県">福井県</option>
+											<option value="山梨県">山梨県</option>
+											<option value="長野県">長野県</option>
+											<option value="岐阜県">岐阜県</option>
+											<option value="静岡県">静岡県</option>
+											<option value="愛知県">愛知県</option>
+											<option value="三重県">三重県</option>
+											<option value="滋賀県">滋賀県</option>
+											<option value="京都府">京都府</option>
+											<option value="大阪府">大阪府</option>
+											<option value="兵庫県">兵庫県</option>
+											<option value="奈良県">奈良県</option>
+											<option value="和歌山県">和歌山県</option>
+											<option value="鳥取県">鳥取県</option>
+											<option value="島根県">島根県</option>
+											<option value="岡山県">岡山県</option>
+											<option value="広島県">広島県</option>
+											<option value="山口県">山口県</option>
+											<option value="徳島県">徳島県</option>
+											<option value="香川県">香川県</option>
+											<option value="愛媛県">愛媛県</option>
+											<option value="高知県">高知県</option>
+											<option value="福岡県">福岡県</option>
+											<option value="佐賀県">佐賀県</option>
+											<option value="長崎県">長崎県</option>
+											<option value="熊本県">熊本県</option>
+											<option value="大分県">大分県</option>
+											<option value="宮崎県">宮崎県</option>
+											<option value="鹿児島県">鹿児島県</option>
+											<option value="沖縄県">沖縄県</option>
 
-											<option value="1">北海道</option>
-											<option value="2">青森県</option>
-											<option value="3">岩手県</option>
-											<option value="4">宮城県</option>
-											<option value="5">秋田県</option>
-											<option value="6">山形県</option>
-											<option value="7">福島県</option>
-											<option value="8">東京都</option>
-											<option value="9">神奈川県</option>
-											<option value="10">埼玉県</option>
-											<option value="11" selected="selected">千葉県</option>
-											<option value="12">茨城県</option>
-											<option value="13">栃木県</option>
-											<option value="14">群馬県</option>
-											<option value="15">山梨県</option>
-											<option value="16">新潟県</option>
-											<option value="17">長野県</option>
-											<option value="18">富山県</option>
-											<option value="19">石川県</option>
-											<option value="20">福井県</option>
-											<option value="21">愛知県</option>
-											<option value="22">岐阜県</option>
-											<option value="23">静岡県</option>
-											<option value="24">三重県</option>
-											<option value="25">大阪府</option>
-											<option value="26">兵庫県</option>
-											<option value="27">京都府</option>
-											<option value="28">滋賀県</option>
-											<option value="29">奈良県</option>
-											<option value="30">和歌山県</option>
-											<option value="31">鳥取県</option>
-											<option value="32">島根県</option>
-											<option value="33">岡山県</option>
-											<option value="34">広島県</option>
-											<option value="35">山口県</option>
-											<option value="36">徳島県</option>
-											<option value="37">香川県</option>
-											<option value="38">愛媛県</option>
-											<option value="39">高知県</option>
-											<option value="40">福岡県</option>
-											<option value="41">佐賀県</option>
-											<option value="42">長崎県</option>
-											<option value="43">熊本県</option>
-											<option value="44">大分県</option>
-											<option value="45">宮崎県</option>
-											<option value="46">鹿児島県</option>
-											<option value="47">沖縄県</option>
 										</select>
 									</td>
 									<td>
-										<button>変更</button>
+										<button type="submit">変更</button>
+										</form>
 									</td>
 								</tr>
 							</table>
@@ -279,7 +292,7 @@
 								</tr>
 							</table> -->
 
-						</form>
+
 					</div>
 
 				</section>
--- View/Member/sys_setting.php
+++ View/Member/sys_setting.php
@@ -10,255 +10,62 @@
   //設定②SQLインジェクション対策
   $pdo -> setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
 
+  $user_id = "";
+	if( isset( $_POST["user_id"] ) ){
+	$user_id = $_POST['user_id'];
+  }
+
+  $mail_address = "";
+	if( isset( $_POST["mail_address"] ) ){
+	$mail_address = $_POST['mail_address'];
+  }
+
+  $password = "";
+	if( isset( $_POST["password"] ) ){
+	$password = $_POST['password'];
+  }
+
+  $newPassword = "";
+	if( isset( $_POST["newPassword"] ) ){
+	$newPassword = $_POST['newPassword'];
+  }
+
+  $NnewPassword = "";
+	if( isset( $_POST["NnewPassword"] ) ){
+	$NnewPassword = $_POST['NnewPassword'];
+  }
+
+  $name = "";
+	if( isset( $_POST["name"] ) ){
+	$name = $_POST['name'];
+  }
+
+  $nameKana = "";
+	if( isset( $_POST["nameKana"] ) ){
+	$nameKana = $_POST['nameKana'];
+  }
+
+  $tel = "";
+	if( isset( $_POST["tel"] ) ){
+	$tel = $_POST['tel'];
+  }
+
+  $Prefectures = "";
+	if( isset( $_POST["Prefectures"] ) ){
+	$Prefectures = $_POST['Prefectures'];
+  }
+
+
+  echo $user_id;
+  echo $mail_address;
+  echo $password;
+  echo $newPassword;
+  echo $NnewPassword;
+  echo $name;
+  echo $nameKana;
+  echo $tel;
+  echo $Prefectures;
+
+
+
 ?>
-
-<!-- ▼表示▼ -->
-<!DOCTYPE html>
-<html lang="ja">
-	<head>
-		<!-- 定義 -->
-		<meta http-equiv="content-type" content="text/html; charset=utf-8">
-		<meta name="viewport" content="width=device-width,initial-scale=1.0">
-		<meta http-equiv="content-style-type" content="text/css">
-		<meta http-equiv="Content-Script-Type" content="text/javascript" />
-		<!-- 読み込み -->
-		<link rel="stylesheet" href="../.././css/Bace/normalize.css">
-		<link rel="stylesheet" href="../.././css/Bace/base.css">
-		<link rel="stylesheet" href="../.././css/layout.css">
-		<link rel="stylesheet" href="../.././css/Common/common.css">
-		<link rel="stylesheet" href="../../css/Member/setting.css">
-
-		<title>ユーザー情報変更</title>
-	</head>
-	<body>
-		<main>
-			<article class="l-wrapper">
-<?php
-	require_once "./../parts/header.html";
-?>
-
-				<section class="l-stepHead">
-					<h1>ユーザー情報変更</h1>
-				</section>
-
-				<section class="l-stepMain">
-					<div class="inn-wrapper">
-						<h2 class="set-H2">会員登録内容変更</h2>
-						<form action="complete.php" method="post" class="userForm">
-							<table>
-								<tr>
-									<td colspan="2">
-										ユーザー情報
-									</td>
-								</tr>
-								<tr>
-									<td class="l-tdWidth1">
-										ユーザネーム
-									</td>
-									<td class="l-tdWidth2">
-										<input type="text" name="userName" placeholder="えはら">
-									</td>
-								</tr>
-								<tr>
-									<td>
-										ユーザID
-									</td>
-									<td>
-										<input type="text" name="id" placeholder="koichi123"><span class="inputMsg">※半角英数</span>
-									</td>
-								</tr>
-								<tr>
-									<td>
-										メールアドレス
-									</td>
-									<td>
-										<input type="text" name="address" placeholder="address">
-									</td>
-								</tr>
-								<tr>
-									<td>
-										パスワード
-									</td>
-									<td>
-										<input type="password" name="password" placeholder="古いパスワード">
-										<input type="password" name="newPassword" placeholder="新しいパスワード">
-										<input type="password" name="NnewPassword" placeholder="新しいパスワード(再入力)">
-									</td>
-								</tr>
-								<tr>
-									<td rowspan="2">
-										お名前
-									</td>
-									<td class="username">
-										姓<input type="text" name="familyName" placeholder="江原">
-										名<input type="text" name="firstName" placeholder="孝一">
-									</td>
-								</tr>
-								<tr>
-									<td class="username">
-										<span>ｾｲ</span><input type="text" name="familyNameKana" placeholder="エハラ">
-										<span>ﾒｲ</span><input type="text" name="firstNameKana" placeholder="コウイチ">
-									</td>
-								</tr>
-								<tr>
-									<td>
-										電話番号
-									</td>
-									<td>
-										<input type="text" name="tel" placeholder="09019799681">
-									</td>
-								</tr>
-								<tr>
-									<td>
-										性別
-									</td>
-									<td>
-										男
-									</td>
-								</tr>
-								<tr>
-									<td>
-										生年月日
-									</td>
-									<td>
-										1994年06月20日
-									</td>
-								</tr>
-								<tr>
-									<td>
-										都道府県
-									</td>
-									<td>
-										<select name="Prefectures">
-											<option value="">都道府県の選択</option>
-
-											<option value="1">北海道</option>
-											<option value="2">青森県</option>
-											<option value="3">岩手県</option>
-											<option value="4">宮城県</option>
-											<option value="5">秋田県</option>
-											<option value="6">山形県</option>
-											<option value="7">福島県</option>
-											<option value="8">東京都</option>
-											<option value="9">神奈川県</option>
-											<option value="10">埼玉県</option>
-											<option value="11" selected="selected">千葉県</option>
-											<option value="12">茨城県</option>
-											<option value="13">栃木県</option>
-											<option value="14">群馬県</option>
-											<option value="15">山梨県</option>
-											<option value="16">新潟県</option>
-											<option value="17">長野県</option>
-											<option value="18">富山県</option>
-											<option value="19">石川県</option>
-											<option value="20">福井県</option>
-											<option value="21">愛知県</option>
-											<option value="22">岐阜県</option>
-											<option value="23">静岡県</option>
-											<option value="24">三重県</option>
-											<option value="25">大阪府</option>
-											<option value="26">兵庫県</option>
-											<option value="27">京都府</option>
-											<option value="28">滋賀県</option>
-											<option value="29">奈良県</option>
-											<option value="30">和歌山県</option>
-											<option value="31">鳥取県</option>
-											<option value="32">島根県</option>
-											<option value="33">岡山県</option>
-											<option value="34">広島県</option>
-											<option value="35">山口県</option>
-											<option value="36">徳島県</option>
-											<option value="37">香川県</option>
-											<option value="38">愛媛県</option>
-											<option value="39">高知県</option>
-											<option value="40">福岡県</option>
-											<option value="41">佐賀県</option>
-											<option value="42">長崎県</option>
-											<option value="43">熊本県</option>
-											<option value="44">大分県</option>
-											<option value="45">宮崎県</option>
-											<option value="46">鹿児島県</option>
-											<option value="47">沖縄県</option>
-										</select>
-									</td>
-								</tr>
-							</table>
-							<h2 class="set-H2"></h2>
-							<table>
-								<tr>
-									<td colspan="2">
-										お支払方法
-									</td>
-								</tr>
-								<tr>
-									<td class="l-tdWidth1">
-										クレジット
-									</td>
-									<td class="l-tdWidth2">
-										<select class="" name="">
-											<option>未設定</option>
-											<option>MasterCard</option>
-											<option>Visa</option>
-										</select>
-									</td>
-								</tr>
-								<tr>
-									<td>
-										種類
-									</td>
-									<td>
-										<select class="" name="">
-											<option>種類1</option>
-											<option>種類2</option>
-										</select>
-									</td>
-								</tr>
-								<tr>
-									<td>
-										番号
-									</td>
-									<td>
-										<select class="" name="">
-											<option>種類1</option>
-											<option>種類2</option>
-										</select>
-									</td>
-								</tr>
-								<tr>
-									<td>
-										有効期限
-									</td>
-									<td>
-										<select class="" name="">
-											<option>種類1</option>
-											<option>種類2</option>
-										</select>
-									</td>
-								</tr>
-								<tr>
-									<td>
-										セキュリティコード
-									</td>
-									<td>
-										<input type="text" name="secureCode">
-									</td>
-								</tr>
-							</table>
-
-							<div class="l-submitBox">
-								<p>
-									入力内容に間違いがなければ「変更する」ボタンをクリックしてください
-								</p>
-							<input type="submit" value="変更する">
-							</div>
-
-						</form>
-					</div>
-					
-				</section>
-			</article>
-		</main>
-
-	</body>
-</html>
-<!-- ▲表示▲ -->
